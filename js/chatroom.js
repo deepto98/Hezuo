8357a6e6653434372b347859eab32c114580e0b0
deepto <ddepp98@outlook.com>
2023-02-14 18:48:53 +0530

    added participant to dom (when I join and someone else joins)

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,5 +1,7 @@
 import { AGORA_APP_ID } from "../env.js";
 
+let messages = document.getElementById("chatroom_chatwindow");
+messages.scrollTop = messages.scrollHeight;
 // console.log(AGORA_APP_ID)
 let agoraAppId = AGORA_APP_ID;
 let token = null;
@@ -19,13 +21,26 @@ let startRoom = async () => {
     console.log("Data", data);
   });
 
+  channel.on("MemberJoined", (memberId) => {
+    addParticipantToDom(memberId);
+  });
+
+  let addParticipantToDom = async (memberId) => {
+    let membersWrapper = document.getElementById("chatroom_participants");
+    let memberItem = `            <div class="member_wrapper">
+                                      <span class="active_identifier"></span>
+                                      <p>${memberId}</p>
+                                  </div>`;
+    membersWrapper.innerHTML += memberItem;
+  };
+
   let addMessageToDom = (messageData, memberId) => {
-    let messagesWrapper = document.getElementById('chatroom_chatwindow');
-    let message = `<div class="message_wrapper">
+    let messages = document.getElementById("chatroom_chatwindow");
+    let messageWrapper = `<div class="message_wrapper">
                         <p>${memberId}</p>
                         <p>${messageData}</p>
                     </div>`;
-    messagesWrapper.insertAdjacentHTML("beforeend", message);
+    messages.insertAdjacentHTML("beforeend", messageWrapper);
   };
 
   let sendMessage = async (e) => {
@@ -38,6 +53,15 @@ let startRoom = async () => {
 
   let chatroomForm = document.getElementById("chatroom_message_form");
   chatroomForm.addEventListener("submit", sendMessage);
+
+  let getParticipants = async (e) => {
+    let participants = await channel.getMembers();
+    console.log(participants);
+    participants.forEach((participant, index, array) => {
+      addParticipantToDom(participant);
+    });
+  };
+  getParticipants();
 };
 
 startRoom();
